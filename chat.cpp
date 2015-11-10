894ae8c516a3d68b68981d9c57b3174c416b1560
sameragi <namet42@yahoo.co.jp>
2015-11-11 00:00:01 +0900

    edit code

--- chat.cpp
+++ chat.cpp
@@ -21,8 +21,8 @@
 #include <unistd.h>
 
 #define log_file_name "log"
-//#define target_ip "192.168.1.26"
-#define target_ip "127.0.0.1"
+#define target_ip "192.168.1.26"
+//#define target_ip "127.0.0.1"
 
 using namespace std;
 
@@ -35,89 +35,88 @@ class receiver {
 	sockaddr_in read_addr;
 	sockaddr_in write_addr;
 	socklen_t write_addr_len = sizeof(write_addr);
-public:
-	receiver(int sock, sockaddr_in &addr) {
 
-		re_sock = sock;
-		read_addr = addr;
+public:
+	receiver() {
+		bzero((char *)&read_addr, sizeof(read_addr));
+		bzero((char *)&write_addr, sizeof(write_addr));
+		re_sock = socket(AF_INET, SOCK_STREAM, 0);
+		read_addr.sin_family = AF_INET;
+		read_addr.sin_port = htons(4000);
 		inet_aton("INADDR_ANY", &read_addr.sin_addr);
 
 		bind(re_sock, (sockaddr *)&read_addr, sizeof(read_addr));
-		listen(re_sock, 1);
+		//listen(re_sock, 1);
 		//re_new_sock = accept(re_sock, (sockaddr *)&write_addr, &write_addr_len);
 	}
 
-	void read_message() {
+	void operator() () {
+		listen(re_sock, 1);
 		re_new_sock = accept(re_sock, (sockaddr *)&write_addr, &write_addr_len);
-
-	//void operator() () {
-		//while(1) {
+		while(1) {
 			char re_message[32];
 			mtx2.lock();
 			recv(re_new_sock, re_message, sizeof(re_message), 0);
 			mtx2.unlock();
+
+			mtx2.lock();
 			cout << "receive message >> " << re_message << endl;
-		//}
+			mtx2.unlock();
+		}
 	}
+
+	~receiver() { close(re_sock); close(re_new_sock); }
 };
 
 
 class transmitter {
 	int tr_sock = 0;
 	sockaddr_in tr_addr;
+
 public:
-	transmitter(int &sock, sockaddr_in &addr) {
-		tr_sock = sock;
-		tr_addr = addr;
+	transmitter() {
+		bzero((char *)&tr_addr, sizeof(tr_addr));
+		tr_sock = socket(AF_INET, SOCK_STREAM, 0);
+		tr_addr.sin_family = AF_INET;
+		tr_addr.sin_port = htons(4000);
 		tr_addr.sin_addr.s_addr = inet_addr(target_ip);
 		//connect(tr_sock, (sockaddr *)&tr_addr, sizeof(tr_addr));
 	}
 
-	//void write_message() {
 	void operator () () {
 		connect(tr_sock, (sockaddr *)&tr_addr, sizeof(tr_addr));
 
 		while(1) {
-		char tr_message[32];
-		cout << "input message >> ";
-		cin >> tr_message;
-		//lock_guard<mutex> lock(mtx);
-		mtx1.lock();
-		send(tr_sock, tr_message, sizeof(tr_message), 0);
-		mtx1.unlock();
+			char tr_message[32];
+			//lock_guard<mutex> lock(mtx1);
+			mtx1.lock();
+			cout << "input message >> ";
+			cin >> tr_message;
+			mtx1.unlock();
+
+			mtx1.lock();
+			send(tr_sock, tr_message, sizeof(tr_message), 0);
+			mtx1.unlock();
 		}
 	}
-};
 
-void func(receiver &re_obj) {
-	while(1) {
-		re_obj.read_message();
-	}
-}
+	~transmitter() { close(tr_sock); }
+};
 
 
 int main()
 {
-	int sock;
-	sockaddr_in addr;
+	receiver re_obj;
+	thread th1(ref(re_obj));
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
+	cout << "target ip >> " << endl;
+	//cin >> 
 
-	bzero((char *)&addr, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(4000);
-	
-	receiver re_obj(sock, addr);
-	cout << "check" << endl;
-	transmitter tr_obj(sock, addr);
-
-	thread th1(func, ref(re_obj));
+	transmitter tr_obj;
 	thread th2(ref(tr_obj));
 
 	th1.join();
 	th2.join();
-
-	close(sock);
 }
 
 
